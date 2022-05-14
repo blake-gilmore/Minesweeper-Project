057f156871150326895e134c9d55a4d105800e4f
blake-gilmore <80655017+blake-gilmore@users.noreply.github.com>
2022-05-13 19:49:02 -0700

    Wait 10 seconds

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <Windows.h>
 #include <iostream>
+#include <winuser.h>
+#include <chrono>
+#include <thread>
 
 int main()
 {
     tagMOUSEINPUT myInput;
+    
+    using namespace std::chrono_literals;
+
+    std::this_thread::sleep_for(10s);
+    std::this_thread::sleep_until(std::chrono::system_clock::now() + 1s);
+    
     std::cout << "Hello Tiffanie.\n";
     return 0;
 }
