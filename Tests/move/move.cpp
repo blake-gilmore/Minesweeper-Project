1bd28b45078679b94219de3ea8042ca749498429
blake-gilmore <80655017+blake-gilmore@users.noreply.github.com>
2022-05-17 02:12:46 -0700

    Extra Tests

--- Tests/move/move.cpp
+++ Tests/move/move.cpp
@@ -17,15 +17,29 @@ void printSelection()
     std::cout << "Select any number to have the computer click. Otherwise, select '9'.";
 
 }
-
+void moveMouse()
+{
+    int xCoord(0);
+    {
+        while (xCoord < 1000)
+        {
+            SetCursorPos(xCoord, 500);
+            Sleep(1);
+            xCoord++;
+        }
+        
+    }
+}
 int main()
 {
+
     int selection;
     printSelection();
     std::cin >> selection;
     while(selection != 9)
     {
         Sleep(2000);
+        moveMouse();
         leftClick();
         printSelection();
         std::cin >> selection;
