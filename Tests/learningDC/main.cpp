abf2052e29bd3fc9018d14653d12decb2212aa9a
blake-gilmore <80655017+blake-gilmore@users.noreply.github.com>
2022-05-23 12:55:55 -0700

    Added Class

--- Tests/learningDC/main.cpp
+++ Tests/learningDC/main.cpp
@@ -1,9 +1,55 @@
 #define UNICODE
 #include <windows.h>
 #include <Gdiplus.h>
+#include <iostream>
+class minesweeperGame
+{
+public:
+    minesweeperGame();
+    void initializeGame();
+    bool MapGame();
+private:
+    LPCWSTR windowName = L"Microsoft Minesweeper";
+    BYTE bitPointer;
+    HWND initWindow;
+    HDC windowDC;
+    HDC virtualDC;
+    int maxWidth;
+    int maxHeight;
+    RECT rect;
+};
+minesweeperGame::minesweeperGame()
+{
+    maxWidth = 0;
+    maxHeight = 0;
+    return;
+}
+void minesweeperGame::initializeGame()
+{
+    initWindow = FindWindow(NULL, windowName);
+    while (initWindow == NULL)
+    {
+        std::cout << "Please start the game. Window cannot be found.";
+        Sleep(1000);
+        initWindow = FindWindow(NULL, windowName);
+    }
+    std::cout << "Window Found!\n";
+    return;
+}
+bool minesweeperGame::MapGame()
+{
+    windowDC = GetDC(initWindow);
+    virtualDC = CreateCompatibleDC(windowDC);
+    
+}
 
 int main()
 {
+    minesweeperGame game;
+    game.initializeGame();
+
+
+
     BYTE* bitPointer;
     RECT rect;
     LPCWSTR windowName = L"Microsoft Minesweeper";
@@ -17,6 +63,6 @@ int main()
     HBITMAP mainBitmap = CreateCompatibleBitmap(windowDC, width, height);
     HGDIOBJ newDC = SelectObject(virtualDC, mainBitmap);
     
-
+    ReleaseDC()
     return 0;
 }
